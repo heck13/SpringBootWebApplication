ff99af3d07666ce8ecdd9c1c40cc9fa88d6b24ad
personAM <anatolschiopu0@gmail.com>
2018-11-15 01:09:15 +0200

    controller

---
+++ src/main/java/com/anatol/springboot/web/springbootwebapplication/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package com.anatol.springboot.web.springbootwebapplication.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String loginMessage() {
+        return "Hello Anatol ce faci";
+
+    }
+}
